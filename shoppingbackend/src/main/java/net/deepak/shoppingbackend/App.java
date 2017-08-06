e7e22e02c7f7b8033fc7a712d54c76cbff1efeb8
deepak mishra <sirdeepakmishra@gmail.com>
2017-08-06 21:31:04 +0530

    first commit of project

---
+++ shoppingbackend/src/main/java/net/deepak/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.deepak.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

0b84ee6ae94052997e559ab950e85e6906bc173d
deepak mishra <sirdeepakmishra@gmail.com>
2017-08-15 11:22:41 +0530

    successfully loaded static resources

---
+++ onlineshopping/src/main/java/net/deepak/onlineshopping/controller/PageController.java
@@ -0,0 +1,46 @@
+package net.deepak.onlineshopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+	
+	@RequestMapping(value={"/","/home","/index"})
+	
+	public ModelAndView  index(){
+		ModelAndView mv= new ModelAndView("page");
+		mv.addObject("greeting","welcome to spring web mvc by deepak");
+		return mv;
+	}
+	
+	
+	@RequestMapping(value="/test2")
+	public ModelAndView test2(@RequestParam(value="greeting",required=false)String greeting){
+		if(greeting == null){
+			
+			greeting="deepak2";
+		}
+		ModelAndView mv= new ModelAndView("page");
+		mv.addObject("greeting",greeting);
+		return mv;
+	}
+	
+	
+	
+	@RequestMapping(value="/test/{greeting}")
+	public ModelAndView test(@PathVariable("greeting")String greeting){
+		if(greeting == null){
+			
+			greeting="deepak";
+		}
+		ModelAndView mv= new ModelAndView("page");
+		mv.addObject("greeting",greeting);
+		return mv;
+	}
+	
+
+}
---
+++ onlineshopping/src/main/java/net/deepak/onlineshopping/controller/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author DEEPAK
+ *
+ */
+package net.deepak.onlineshopping.controller;
